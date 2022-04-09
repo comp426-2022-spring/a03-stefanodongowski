3a8c11bd7c2d49f5ffa11729585dd5236bc82b11
Stefano Dongowski <stefano@unc.edu>
2022-04-09 15:38:44 -0400

    fixed port issue

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ function countFlips(array) {
 
 function flipACoin(call) {
     const flip = coinFlip();
-    const result = (call === flip) ? true : false;
+    const result = (call === flip) ? win : lose;
     return { "call": call, "flip":flip, "result":result }
 }
