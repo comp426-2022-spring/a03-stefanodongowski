b1ee0704074bcfc641f2fb581954e03aae7be8fa
Stefano Dongowski <stefano@unc.edu>
2022-04-09 15:26:02 -0400

    flip and flips APIs created

--- server.js
+++ server.js
@@ -18,7 +18,48 @@ app.get('/app', (req, res) => {
     res.type("text/plain")
 })
 
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({ "flip" : coinFlip()})
+})
+
+app.get('/app/flips/:number', (req, res) => {
+    var flips = coinFlips(req.params.number);
+    res.status(200).json({ "raw" : flips,
+                           "summary" : countFlips(flips)})
+})
+
 app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
 })
+
+function coinFlip() {
+    var flip = Math.random();
+    if (flip < 0.5) {
+      return "tails"
+    }
+    return "heads"
+}
+
+function coinFlips(numFlips) {
+    var flips = new Array();
+    for (let i=0;i<numFlips;i++) {
+      flips.push(coinFlip());
+    }
+    return flips;
+}
+
+function countFlips(array) {
+    var heads = 0;
+    var tails = 0;
+    for (const flip of array) {
+      (flip === "heads") ? heads ++ : tails ++;
+}
+    return { "tails":tails, "heads":heads }
+  }
+
+function flipACoin(call) {
+    const flip = coinFlip();
+    const result = (call === flip) ? true : false;
+    return { "call": call, "flip":flip, "result":result }
+}
