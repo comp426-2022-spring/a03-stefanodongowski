c8ad72272b8571fe36ce8210c62acb2970ae4616
Stefano Dongowski <stefano@unc.edu>
2022-04-09 14:38:17 -0400

    added default endpoint

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const server = app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 })
 
+app.get('/app', (req, res) => {
+    res.status(200).send("OK")
+    res.type("text/plain")
+})
+
 app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
